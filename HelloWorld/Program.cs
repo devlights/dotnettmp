9dd655d307758cb3677b19b4133b627088d06d92
devlights <gsf.zero1@devlights.onmicrosoft.com>
2020-11-06 06:18:08 +0000

    Update HelloWorld

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,31 @@
 using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace HelloWorld
 {
     class Program
     {
-        static void Main()
+        static async Task Main()
         {
-            Console.WriteLine("Hello World");
+            await foreach (var c in GenMessage())
+            {
+                Console.Write(c);
+            }
+
+            Console.WriteLine("");
+        }
+
+        static async IAsyncEnumerable<char> GenMessage()
+        {
+            var rnd = new Random();
+            foreach (var c in "HelloWorld")
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(rnd.Next(50)));
+                yield return c;
+            }
+
+            yield break;
         }
     }
 }
