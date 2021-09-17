e799e023df9516611be1245ea1979d42f1d57b14
devlights <gsf.zero1@devlights.onmicrosoft.com>
2021-09-17 01:21:38 +0000

    Update

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnettmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
